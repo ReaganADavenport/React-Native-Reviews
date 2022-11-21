f5434dde7d6421001fc3a9b2f60d8800e89d3a90
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-11-21 15:14:59 -0500

    learned how to use state, make buttons, and style thos buttons

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Shaun');
+  const [person, setPerson] = useState({ name: "Mario", age: 40});
+  
+  const clickHandler = () => {
+    setName('Chun-Li');
+    setPerson({ name: 'Luigi', age: 45});
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text></Text>
-        <Text></Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and he is {person.age} years old</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update State" onPress={clickHandler} color='#fff'/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -21,4 +31,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  buttonContainer: {
+    backgroundColor: '#54E1F2',
+    borderRadius: 5,
+    marginTop: 20,
+  }
 });
