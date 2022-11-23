678bd07492acd6275af826a3013cf1d0256abe16
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-11-23 14:27:13 -0500

    created first components, header and todoitem, making todo list, can delete items from list by touching them.

--- App.js
+++ App.js
@@ -1,49 +1,38 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+
+import Header from './components/header';
+import TodoItem from './components/todoitem';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
-  ]);
   
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople( (prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
+  const [todos, setTodos] = useState([
+    { text: 'buy tea', key: '1'},
+    { text: 'create an app', key: '2'},
+    { text: 'play on the switch', key: '3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos( (prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
     })
   }
 
   return (
     <View style={styles.container}>
-      
-      <FlatList 
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-            </TouchableOpacity>
-          
-        )}
-      />
-
-
-      {/* <ScrollView>
-        { people.map(item => (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          ))}
-      </ScrollView> */}
-
+      <Header/>
+      <View style={styles.content}>
+        {/*To-do Form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -53,18 +42,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+
+  list:{
+    marginTop: 20,
   }
+
 });
