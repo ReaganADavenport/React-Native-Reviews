ecc2c6c6f94d2ff935d17d96bbce031835e63913
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-11-23 10:35:57 -0500

    learned about touchable components

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -13,7 +13,12 @@ export default function App() {
     { name: 'bowser', id: '7' },
   ]);
   
-  
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople( (prevPeople) => {
+      return prevPeople.filter(person => person.id != id);
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -23,7 +28,10 @@ export default function App() {
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+            </TouchableOpacity>
+          
         )}
       />
 
