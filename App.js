6c3ce527ae7db5d7e2c0bd1c3d41fd37413c8365
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-12-08 16:11:10 -0500

    installed AppLoading, created a fonts folder to put 2 fonts (lora-regular and lora-bold), created 3 screen files (home, about, and reviewDetails) and a folder for them.

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React, {useState} from 'react';
+import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import Home from './screens/home';
+
+const getFonts = () => Font.loadAsync({
+    'lora-regluar': require('./assets/fonts/Lora-Regular.ttf'),
+    'lora-bold': require('./assets/fonts/Lora-Bold.ttf')
+  });
 
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  return (
-    <View>
-      
-    </View>
-  );
+  if (fontsLoaded){
+    return (
+      <Home/>
+     );
+  } else {
+    return(
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.log("Huston, we have a problem.")}
+      ></AppLoading>
+    )
+  }
+  
 }
 
-const styles = StyleSheet.create({
-
-});
