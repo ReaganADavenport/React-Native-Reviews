dce5fcc8017cdf14f68ed622a8bb2e7b4531e6eb
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-12-14 16:49:38 -0500

    added dummy reviews to home page, made it so that I can pass data from home to reviewDetails

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails(){
+export default function ReviewDetails({ route }){
+
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Review Details Screen</Text>
+            <Text style={globalStyles.titleText}>{ route.params.paramKey }</Text>
         </View>
     )
 }
